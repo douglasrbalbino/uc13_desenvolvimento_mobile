b974e32aafb1b713d50e98770cadda338941a1e5
Douglas <doug.balbino@hotmail.com>
2025-09-15 20:06:49 -0300

    removi o floatbutton

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -77,15 +77,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
- 
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'É um botão',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
